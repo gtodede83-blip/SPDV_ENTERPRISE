909819f3ca703ee46260983c3ae40fb75f7a28fd
Wemerson Silva <SEU_EMAIL_DO_GITHUB>
2026-07-01 21:56:27 -0300

    Atualiza server.js

--- server.js
+++ server.js
@@ -736,3 +736,9 @@ app.put("/usuarios/:id/bloquear", async (req,res)=>{
     }
 
 });
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
